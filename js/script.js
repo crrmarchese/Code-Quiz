d5ed08a96f325c1e0161dfdb242a350f2908425e
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-01-17 16:44:59 -0500

    Add quiz score and final score sections

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
 // DOM Elements
-const quizIntroEl = document.querySelector("#start-quiz-about");
+const quizIntroEl = $("#start-quiz-about");
 const startButton = document.querySelector("#start-button");
-const quizSectionEl = document.querySelector("#quiz-questions-all");
-const quizQuestionEl = document.querySelector("#quiz-question");
-const quizQuestionNumberEl = document.querySelector(".quiz-question-number");
-// const quizQuestionNumberEl = document.createElement("span");
-// const answerChoicesEl = document.querySelector("#answer-choices");
+const quizSectionEl = $("#quiz-questions-all");
 const timerCounterEl = document.querySelector("#timer-count");
-// const answerResultEl = document.querySelector("#user-answer-result");
-// const quizContainer = document.querySelector("#quiz-container");
-const divTextStartEl = document.querySelector(".text-start"); //Use this for HTML reset
-const finalScoreEl = document.querySelector("#final-score");
+const answerContainer= $(".answer-container");
+const answerResultEl = $(".user-answer-result");
+const scoreEl = $("#final-score");
+
+// Score
+let score = 0;
+scoreEl.text(score);
 
 //Timer variables
 let timer
 let timerCount; //seconds on timer
 
-
 //Start at first question (index) Keep this!
 let indexCounter = 0;
 
 // Quiz Questions
-let quizQuestionsAll = [
+const quizQuestionsAll = [
     {
         questionNumber: 1,
         question: "What is a block of JavaScript code that can be executed when \"called\" for?",
@@ -59,112 +57,102 @@ let quizQuestionsAll = [
     }
 ]
 
+
 // The init function is called when the page loads 
 function init() {
   startQuizAnimation();
+  storedScore();
 }
 
 // Fade in starting page for quiz
 function startQuizAnimation() {
-    quizIntroEl.classList.add("animate__animated", "animate__fadeIn");
+    quizIntroEl.addClass("animate__animated animate_fadeIn");
 }
 
 // Animation to hide quiz starting section
 function startGame() {
-    // Hide intro to quiz
-    quizIntroEl.classList.remove("animate__fadeIn");
-    quizIntroEl.classList.add("animate__fadeOut");  
+    quizIntroEl.removeClass("animate__fadeIn");
+    quizIntroEl.addClass("animate__fadeOut");  
     quizIntroEl.remove();
+    
     // Show quiz questions section
-    quizSectionEl.classList.remove("d-none");
-    quizSectionEl.classList.add("animate__animated", "animate__fadeInUp");
+    quizSectionEl.removeClass("d-none");
+    quizSectionEl.addClass("animate__animated animate__fadeInUp");
 
-    //Set time on counter
-     timerCount = 75;
+   //Set time on counter
+    timerCount = 75;
 
-     // Start Timer
-    startTimer();
+   //Start Timer
+    startTimer()
 
     //Load Quiz Questions
-    getQuestion()
+    getQuestion();
     
- 
 }
 
 function getQuestion() {
-    // Create Quiz Container
-    let quizContainer = document.createElement("div");
-    divTextStartEl.appendChild(quizContainer);
-    quizContainer.setAttribute("class","quiz-container");
-    quizContainer.setAttribute("class","animate__animated animate__fadeIn");
-
-    // Create Paragraph <p> for questions
+    // Get question number and question from quizQuestionsAll Object
     let currentQuestion = quizQuestionsAll[indexCounter];
-    let paraQuestion = document.createElement("p");
-    paraQuestion.textContent = currentQuestion.question;
-    quizContainer.appendChild(paraQuestion);
-    paraQuestion.setAttribute("id","quiz-question-" + currentQuestion.questionNumber);
-    paraQuestion.setAttribute("class","fw-bold");
-
-    // Create <span> for question numbers
-    let questionId = document.createElement("span");
-    questionId.textContent = currentQuestion.questionNumber;
-    paraQuestion.prepend(questionId);
-    questionId.setAttribute("class", "quiz-question-number me-2");
-
-    // Create <div> for buttons
-    let divElBtn = document.createElement("div");
-    quizContainer.appendChild(divElBtn);
-    divElBtn.setAttribute("class", "button-group-choices");
-
-    // Loop through answers currently only the first one is set
-    currentQuestion.choices.forEach(function(choice){
-        let btnChoices = document.createElement("button");
-        divElBtn.appendChild(btnChoices);
-        btnChoices.setAttribute("class", "btn btn-lg btn-primary ripple-surface");
-        btnChoices.setAttribute("type", "button");
-        btnChoices.setAttribute("value", choice);
-        btnChoices.textContent = choice;
-        // Attach event Listener when creating buttons; 
-        // Check answer when button clicked, move to next question
-        btnChoices.addEventListener("click", btnAnswer);
-    });
-    
+        $("span.quiz-question-number").text(currentQuestion.questionNumber);
+        $("p.quiz-question").text(currentQuestion.question).attr("id","quiz-question-" + (currentQuestion.questionNumber));
+        $(".button-group-choices button:first").text(currentQuestion.choices[0]). attr("value", currentQuestion.choices[0]);
+        $(".button-group-choices button:nth-child(2n)").text(currentQuestion.choices[1]).attr("value", currentQuestion.choices[1]);
+        $(".button-group-choices button:nth-child(3n)").text(currentQuestion.choices[2]).attr("value", currentQuestion.choices[2]);
+        $(".button-group-choices button:last").text(currentQuestion.choices[3]).attr("value", currentQuestion.choices[3]);
+       
+         
+    btnAnswer(); 
 }
 
-function btnAnswer() {
-    // Create div to hold <hr> and answer choice
-    let divSibling = document.querySelector("div.button-group-choices")
-    let divElAnswer = document.createElement('div');
-    divSibling.after(divElAnswer);
-    divElAnswer.setAttribute("class", "answer-container");
+ function btnAnswer() {
+     // Attach event Listener to buttons; Get button value
+        $(".button-group-choices .btn").on("click", function(event) {
+            let answerClicked = ($(this).attr("value"));
+
+        // Check if answer is correct
+            answerContainer.removeClass("d-none");
+            answerContainer.addClass("animate__animated animate__fadeInUp");
+            if(answerClicked === quizQuestionsAll[indexCounter].correctAnswer) {
+                answerResultEl.text("Correct!");
+                let finalScore = score += 20;
+                scoreEl.text(finalScore);
+
+            } else {
+                answerResultEl.text("Wrong!");
+                timerCount = timerCount - 10; // Wrong answer deduct 10 seconds
+            }
+            if(indexCounter < 5) {
+                transitionQuestion();
+
+            }  else {
+             $("#quiz-score-container").removeClass("d-none");   
+            }    
+                        
+        });
     
-    // Create Horizontal rule <hr>
-    let hrRule = document.createElement("hr");
-    divElAnswer.appendChild(hrRule);
-    hrRule.setAttribute("class", "text-primary");
+ }
 
-    // Create <p> with answer
-    let answerResultEl = document.createElement("p");
-    divElAnswer.appendChild(answerResultEl);
-    answerResultEl.setAttribute("class", "user-answer-result");
+ // Empty quiz container and transition to next question
+ function transitionQuestion() {
+    const quizContainer = function() {
 
-    // Check if answer is correct
-    let answerClicked = (this.value);
-    if(answerClicked === quizQuestionsAll[indexCounter].correctAnswer) {
-        answerResultEl.textContent = "Correct!";
-    } else {
-        answerResultEl.textContent = "Wrong!";
-        timerCount = timerCount - 10; // Wrong answer deduct 10 seconds
-    }
+        setTimeout(function(){
+            console.log("emptying!!");
+            $(".quiz-container").empty();
+        }, 3000);
     
-    //write logic to show if answer is correct
-    //write logic to go to the next question indexCounter ++
-    //clear page with html remove
-    indexCounter ++;
-    getQuestion();
-}
+    } 
+        
 
+        if(indexCounter < 5) {
+            indexCounter++;
+            getQuestion();
+        } else {
+            getFinalScore();
+        }
+         
+   
+ }
 
 //Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
@@ -172,7 +160,7 @@ startButton.addEventListener("click", startGame);
 // Calls init() so that it fires when page opened
 init();
 
-//Timer
+// Timer
 function startTimer() {
     // Sets timer
     timer = setInterval(function() {
@@ -184,3 +172,13 @@ function startTimer() {
       }
     }, 1000);
 }
+
+// Show final score section
+function getFinalScore() {
+    $("#quiz-score-container").removeClass("d-none");
+}
+
+function storedScore() {
+    // Stringify and set key in localStorage to scoring
+    localStorage.setItem("score", JSON.stringify(score));
+  }
